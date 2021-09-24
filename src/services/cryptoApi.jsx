2934e1bdb459ec7484307b7e60eff2dc87f3e81a
Chadwell Clark <wcclark23@comcast.net>
2021-09-23 19:22:23 -0500

    Initial Commit- HomePage displaying cryptos

---
+++ src/services/cryptoApi.jsx
@@ -0,0 +1,23 @@
+import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
+
+const cryptoApiHeaders = {
+    'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+     'x-rapidapi-key': 'fe42040162mshbfad171808f4355p14c9e0jsne5648108bad6'
+}
+const baseUrl = 'https://coinranking1.p.rapidapi.com'
+
+const createRequest = (url) => ({url, headers: cryptoApiHeaders})
+
+export const cryptoApi = createApi({
+    reducerPath: 'cryptoApi',
+    baseQuery: fetchBaseQuery({baseUrl}),
+    endpoints:(builder) => ({
+        getCryptos: builder.query({
+            query: () => createRequest('/coins')
+        })
+    })
+})
+
+export const {
+    useGetCryptosQuery,
+} = cryptoApi;
